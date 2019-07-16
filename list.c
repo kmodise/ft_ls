8d8c88e000d690840eec4c31474b612b8cbc4422
Keabetswe Justice MODISE <kmodise@c5r10s7.wethinkcode.co.za>
2019-07-16 10:45:35 +0200

    list mod

--- list.c
+++ list.c
@@ -6,32 +6,16 @@
 /*   By: kmodise <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/15 17:43:07 by kmodise           #+#    #+#             */
-/*   Updated: 2019/07/16 09:37:23 by kmodise          ###   ########.fr       */
+/*   Updated: 2019/07/16 10:34:30 by kmodise          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int     lst(const char *filename)
+void				print(char **files)
 {
-	DIR             *dir;
-	struct dirent   *sd;
-	char            **files;
-	int             i;
+	int				i;
 
-	i = 0;
-	files = (char **)malloc(sizeof(char) * 4294967295);
-	if (files == NULL)
-		return (-1);
-	dir = opendir(filename);
-	if (dir == NULL)
-		exit(1);
-	while ((sd = readdir(dir)) != NULL)
-	{
-		files[i] = sd->d_name;
-		i++;
-	}
-	files[i] = NULL;
 	i = 0;
 	while (files[i] != NULL)
 	{
@@ -42,7 +26,28 @@ int     lst(const char *filename)
 		}
 		i++;
 	}
+}
+
+int					lst(const char *filename)
+{
+	DIR				*dir;
+	struct dirent	*sd;
+	char			**files;
+	int				i;
+
+	i = 0;
+	files = (char **)malloc(sizeof(char) * 4294967295);
+	if (files == NULL)
+		return (-1);
+	dir = opendir(filename);
+	if (dir == NULL)
+		exit(1);
+	while ((sd = readdir(dir)) != NULL)
+		files[i++] = sd->d_name;
+	files[i] = NULL;
+	i = 0;
+	print(files);
 	ft_putchar('\n');
-	free (files);
+	free(files);
 	return (0);
 }
